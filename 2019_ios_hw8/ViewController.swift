7bbb5d3f1dcf60965007c8b0e037cf9adb6927a1
creek0810 <creek880810@gmail.com>
2019-04-29 23:47:52 +0800

    Initial Commit

---
+++ 2019_ios_hw8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2019_ios_hw8
+//
+//  Created by 王心妤 on 2019/4/29.
+//  Copyright © 2019年 river. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ 2019_ios_hw8Tests/_019_ios_hw8Tests.swift
@@ -0,0 +1,34 @@
+//
+//  _019_ios_hw8Tests.swift
+//  2019_ios_hw8Tests
+//
+//  Created by 王心妤 on 2019/4/29.
+//  Copyright © 2019年 river. All rights reserved.
+//
+
+import XCTest
+@testable import _019_ios_hw8
+
+class _019_ios_hw8Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
